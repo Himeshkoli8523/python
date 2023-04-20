5cea579eaacfafdda450f5ae9f2c88ec62a8d8b2
Himeshkoli8523 <121806382+Himeshkoli8523@users.noreply.github.com>
2023-04-20 21:20:15 +0530

    day 2 assingment

---
+++ fac.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number to get factorial :"))
+fac = 1
+while n>0:
+    fac = n*fac
+    n = n - 1
+print("the factorial is : ",fac)
---
+++ mul.py
@@ -0,0 +1,4 @@
+tab = int(input("Enter the multiplication table number :"))
+i = 1
+for i in range(1,11):
+  print(tab*i)
