dbfa6cc737a6c19092ff0978002890f34116e015
Himeshkoli8523 <121806382+Himeshkoli8523@users.noreply.github.com>
2023-04-30 00:17:44 +0530

    Update tuple_index.py
    after changing tuple index program

--- tuple_index.py
+++ tuple_index.py
@@ -3,9 +3,7 @@ list2 = intstr.split()
 if len(list2) == 10:
     tuple1 = tuple(map(int, list2))
     value = int(input("Enter the index of the element to delete: "))
-    list1 = list(tuple1)
-    del list1[value]
-    tuple1 = tuple(list1)
+    tuple1 = tuple1[:value]+tuple1[value+1:]
     print("After deleting the element:", tuple1)
 else:
     print("Invalid input. Please enter 10 elements.")
