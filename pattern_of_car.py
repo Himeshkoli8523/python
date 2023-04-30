0103317c9065f921c186acd9e82a6d458897a8a8
Himeshkoli8523 <121806382+Himeshkoli8523@users.noreply.github.com>
2023-04-30 22:03:35 +0530

    Add files via upload
    day 10 assignment

---
+++ pattern_of_car.py
@@ -0,0 +1,69 @@
+# pattern of car
+
+#for first line
+for i in range(12):
+    print(" ", end="")
+print("*" * 25)
+#for second line
+for i in range(11):
+    print(" ", end="")
+print("*", end="")
+for i in range(12):
+    print(" ", end="")
+print("*", end="")
+for i in range(12):
+    print(" ", end="")
+print("*")
+
+#for third line
+for i in range(10):
+    print(" ", end="")
+print("*", end="")
+for i in range(13):
+    print(" ", end="")
+print("*", end="")
+for i in range(13):
+    print(" ", end="")
+print("*")
+
+#for two line of car body
+for i in range(50):
+    print("*",end="")
+print()
+for i in range(50):
+    print("*",end="")
+print()
+# for first line of first wheel
+for i in range(3):
+    print(" ",end="")
+print("*     *",end="")
+# for first line of second wheel
+for i in range(3):
+    print(" ",end="")
+print("*     *",end="")
+
+# for first line of third wheel
+for i in range(8):
+    print(" ",end="")
+print("*     *",end="")
+# for first line of fourth wheel
+for i in range(3):
+    print(" ",end="")
+print("*     *",end="")
+print()
+# for second line of first wheel
+for i in range(3):
+    print(" ",end="")
+print(" *   *",end="")
+# for second line of second wheel
+for i in range(3):
+    print(" ",end="")
+print("  *   *",end="")
+# for second line of third wheel
+for i in range(9):
+    print(" ",end="")
+print(" *   *",end="")
+# for second line of fourth wheel
+for i in range(3):
+    print(" ",end="")
+print("  *   *",end="")
